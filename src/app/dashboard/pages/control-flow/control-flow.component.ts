3a17b7c0f645567cd38340e344d617f6cf836ac9
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-03-21 19:08:03 +0100

    330. Control Flow - @For

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -10,6 +10,8 @@ type Grade = 'A' | 'B' | 'F' | '';
 export default class ControlFlowComponent {
   public showContent = signal(false);
   public grade = signal<Grade>('A');
+  public frameworks = signal(['Angular', 'Vue', 'Svelte', 'Qwik']);
+  public frameworks2 = signal([]);
 
   public toggleContent() {
     this.showContent.update((value) => {
