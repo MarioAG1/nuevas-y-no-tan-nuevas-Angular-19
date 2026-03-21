66df25f7ef580cee152c301a716f1fbaf5548f5e
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-03-21 20:50:26 +0100

    331. Required @Input, esto es antiguo he puesto la version actualizada

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   imports: [],
-  templateUrl: './title.component.html',
+  template: ` <h1 class="text-3xl mb-5">{{ title() }}</h1>`,
 })
-export class TitleComponent { }
+export class TitleComponent {
+  title = input.required<string>();
+}
