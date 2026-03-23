1f519de3fb2c717850a702bc795ccb7c6cbe6515
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-03-23 11:32:00 +0100

    332. ChangeDetection - onPush

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-change-detection',
-  imports: [],
+  imports: [TitleComponent, JsonPipe],
   templateUrl: './change-detection.component.html',
 })
-export default class ChangeDetectionComponent {}
+export default class ChangeDetectionComponent {
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  constructor() {
+    setTimeout(() => {
+      this.frameworkAsSignal.update((valor) => {
+        valor.name = 'React';
+        return { ...valor };
+      });
+
+      this.frameworkAsSignal.set({ name: 'React', releaseDate: 212 });
+
+      this.frameworkAsProperty.name = 'React';
+      console.log('Hecho');
+    }, 3000);
+  }
+}
