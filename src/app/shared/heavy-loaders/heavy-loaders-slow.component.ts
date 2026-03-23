5747906f9a10117001093ea08a164a5a643e8eac
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-03-23 14:56:50 +0100

    333. Defer Blocks

--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
   imports: [],
   template: `<h1>Hola Mundo</h1>`,
 })
-export class HeavyLoadersSlowComponent {}
+export class HeavyLoadersSlowComponent {
+  // @Input({ required: true }) cssClass!: string;
+  cssClass = input.required<string>();
+
+  constructor() {
+    const start = Date.now();
+    while (Date.now() - start < 3000) {}
+
+    console.log('Cargado');
+  }
+}
